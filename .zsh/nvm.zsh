fc739459ea9210ba180cd0849809033a000457fd
Manu <mlorentedev@gmail.com>
2025-03-23 01:17:35 -0600

    bug: fix issue in nvm alias inizialiation

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -1,7 +1,17 @@
-# NVM lazy load
-if [ -s "$HOME/.nvm/nvm.sh" ]; then
-  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-  alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
-  alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
-  alias npm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && npm'
+# NVM (Node Version Manager) Configuration
+# -----------------------------------------
+
+# Set NVM directory
+export NVM_DIR="$HOME/.nvm"
+
+# This loads nvm
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  # Unset any existing nvm alias first to avoid the "defining function based on alias" error
+  unalias nvm 2>/dev/null || true
+  
+  # Load NVM
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  
+  # Load bash completion
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
